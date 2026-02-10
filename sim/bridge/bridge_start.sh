7fd149083ce877f535a596e665fa4e45661db592
efekaanguler <efekaanguler05@gmail.com>
2026-02-10 20:50:58 +0300

    implemented vehicle input

--- sim/bridge/bridge_start.sh
+++ sim/bridge/bridge_start.sh
@@ -3,4 +3,5 @@ ros2 launch carla_ros_bridge carla_ros_bridge.launch.py \
     port:=2000 \
     timeout:=10 \
     synchronous_mode:=true \
-    fixed_delta_seconds:=0.05
+    fixed_delta_seconds:=0.05 \
+    ego_vehicle_role_name:='["ego_vehicle"]'
