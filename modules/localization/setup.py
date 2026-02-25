5e9f5df32b87f750e49d606d047416aff305a759
efekaanguler <efekaanguler05@gmail.com>
2026-02-25 17:05:24 +0300

    updated localization pkg

---
+++ modules/localization/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'localization'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[],
+    py_modules=[
+        'initial_pose_pub',
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Localization bringup/config package (YabLoc-first).',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'initial_pose_pub = initial_pose_pub:main',
+        ],
+    },
+)
