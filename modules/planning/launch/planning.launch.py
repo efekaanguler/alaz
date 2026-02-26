72846a7ae0c9610fec982af6b54a8ecf20f97f94
Mehmet Kekec <kekecmehmet71@gmail.com>
2026-02-26 06:24:12 +0300

    Planning fixed

--- modules/planning/launch/planning.launch.py
+++ modules/planning/launch/planning.launch.py
@@ -7,30 +7,38 @@ import os
 
 
 def generate_launch_description():
-    planning_param_path = os.path.join(
-        get_package_share_directory("planning"), "config", "planning.yaml"
+    behavior_param_path = os.path.join(
+        get_package_share_directory("planning"), "config", "behavior_path_planner.param.yaml"
+    )
+    velocity_smoother_param_path = os.path.join(
+        get_package_share_directory("planning"), "config", "default_velocity_smoother.param.yaml"
     )
 
     return LaunchDescription(
         [
             DeclareLaunchArgument(
-                name="planning_params",
-                default_value=planning_param_path,
-                description="Path to planning parameters file",
+                name="behavior_path_planner_params",
+                default_value=behavior_param_path,
+                description="Path to behavior path planner parameters file",
+            ),
+            DeclareLaunchArgument(
+                name="velocity_smoother_params",
+                default_value=velocity_smoother_param_path,
+                description="Path to velocity smoother parameters file",
             ),
             Node(
-                package="behavior_path_planner",
-                executable="behavior_path_planner_node",
-                name="behavior_path_planner",
-                parameters=[LaunchConfiguration("planning_params")],
+                package="autoware_behavior_path_planner",
+                executable="autoware_behavior_path_planner_node",
+                name="autoware_behavior_path_planner",
+                parameters=[LaunchConfiguration("behavior_path_planner_params")],
                 remappings=[],
                 output="screen",
             ),
             Node(
-                package="motion_velocity_smoother",
-                executable="motion_velocity_smoother_node",
-                name="motion_velocity_smoother",
-                parameters=[LaunchConfiguration("planning_params")],
+                package="autoware_velocity_smoother",
+                executable="velocity_smoother_node",
+                name="velocity_smoother",
+                parameters=[LaunchConfiguration("velocity_smoother_params")],
                 remappings=[],
                 output="screen",
             ),
